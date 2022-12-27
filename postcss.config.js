616f5b89179094757240102e8a2bbcd0d2155196
Baradel-Clement <baradelclement@gmail.com>
2022-12-27 09:46:20 +0100

    .

--- postcss.config.js
+++ postcss.config.js
@@ -7,7 +7,7 @@ module.exports = {
             './pages/**/*.{js,jsx,ts,tsx}',
             './components/**/*.{js,jsx,ts,tsx}',
           ],
-          defaultExtractor: content => content.match(/[\w-:./:]+(?<!:)/g) || [],
+          defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
         },
       ] : undefined,
     'postcss-preset-env',
