c6babad8102d01c36f8615ed79bf127621d724d9
Baradel-Clement <baradelclement@gmail.com>
2022-12-27 09:29:03 +0100

    .

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import { HomeContextProvider } from '../context/Home';
 import { BoardContextProvider } from '../context/Board';
 import { TaskContextProvider } from '../context/Task';
 import { SessionProvider } from 'next-auth/react';
-
+ 
 import '../styles/index.css'
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
