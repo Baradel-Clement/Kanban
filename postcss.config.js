fdb677e35ff9449a662022879b062663f603e5be
Baradel-Clement <baradelclement@gmail.com>
2023-01-02 16:23:23 +0100

    .

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,6 @@
 module.exports = {
-  plugins: [
-    'tailwindcss',
-    process.env.NODE_ENV === 'production' ?
-      [ '@fullhuman/postcss-purgecss', {
-          content: [
-            "./app/**/*.{js,ts,jsx,tsx}", // Note the addition of the `app` directory.
-            "./pages/**/*.{js,ts,jsx,tsx}",
-            "./components/**/*.{js,ts,jsx,tsx}",
-          ],
-          defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-        },
-      ] : undefined,
-    'postcss-preset-env',
-  ],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,46 +1,49 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./ap/**/*.{js,ts,jsx,tsx}", // Note the addition of the `app` directory.
-    "./paes/**/*.{js,ts,jsx,tsx}",
-    "./comonents/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}", // Note the addition of the `app` directory.
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    colors: {
-      black: '#000112',
-      darkBg: '#20212C',
-      darkGrey: '#2B2C37',
-      linesDark: '#3E3F4E',
-      mediumGrey: '#828FA3',
-      linesLight: '#E4EBFA',
-      lightBg: '#F4F7FD',
-      white: '#FFFFFF',
-      purple: '#635FC7',
-      purpleHover: '#A8A4FF',
-      red: '#EA5555',
-      redHover: '#FF9898',
-    },
-    fontFamily: {
-      sans: ['Plus Jakarta Sans', 'sans-serif']
-    },
-    fontSize: {
-      hS: '12px',
-      bL: '13px',
-      hM: '15px',
-      hL: '18px',
-      hXL: '24px',
-    },
-    lineHeight: {
-      S: '15px',
-      M: '19px',
-      L: '23px',
-      XL: '30px',
-    },
-    letterSpacing: {
-      S: '2.4px',
+    extend: {
+      colors: {
+        black: '#000112',
+        darkBg: '#20212C',
+        darkGrey: '#2B2C37',
+        linesDark: '#3E3F4E',
+        mediumGrey: '#828FA3',
+        linesLight: '#E4EBFA',
+        lightBg: '#F4F7FD',
+        white: '#FFFFFF',
+        purple: '#635FC7',
+        purpleHover: '#A8A4FF',
+        red: '#EA5555',
+        redHover: '#FF9898',
+      },
+      fontFamily: {
+        sans: ['Plus Jakarta Sans', 'sans-serif']
+      },
+      fontSize: {
+        hS: '12px',
+        bL: '13px',
+        hM: '15px',
+        hL: '18px',
+        hXL: '24px',
+      },
+      lineHeight: {
+        S: '15px',
+        M: '19px',
+        L: '23px',
+        XL: '30px',
+      },
+      letterSpacing: {
+        S: '2.4px',
+      }
     },
   },
   plugins: [
+    require('flowbite/plugin'),
   ],
   darkMode: 'class',
 }
