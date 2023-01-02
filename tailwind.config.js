ee9d9abd4db6c2a8b32c6a579fb7e4b55d529756
Baradel-Clement <baradelclement@gmail.com>
2023-01-02 16:17:27 +0100

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,40 +6,38 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        black: '#000112',
-        darkBg: '#20212C',
-        darkGrey: '#2B2C37',
-        linesDark: '#3E3F4E',
-        mediumGrey: '#828FA3',
-        linesLight: '#E4EBFA',
-        lightBg: '#F4F7FD',
-        white: '#FFFFFF',
-        purple: '#635FC7',
-        purpleHover: '#A8A4FF',
-        red: '#EA5555',
-        redHover: '#FF9898',
-      },
-      fontFamily: {
-        sans: ['Plus Jakarta Sans', 'sans-serif']
-      },
-      fontSize: {
-        hS: '12px',
-        bL: '13px',
-        hM: '15px',
-        hL: '18px',
-        hXL: '24px',
-      },
-      lineHeight: {
-        S: '15px',
-        M: '19px',
-        L: '23px',
-        XL: '30px',
-      },
-      letterSpacing: {
-        S: '2.4px',
-      }
+    colors: {
+      black: '#000112',
+      darkBg: '#20212C',
+      darkGrey: '#2B2C37',
+      linesDark: '#3E3F4E',
+      mediumGrey: '#828FA3',
+      linesLight: '#E4EBFA',
+      lightBg: '#F4F7FD',
+      white: '#FFFFFF',
+      purple: '#635FC7',
+      purpleHover: '#A8A4FF',
+      red: '#EA5555',
+      redHover: '#FF9898',
+    },
+    fontFamily: {
+      sans: ['Plus Jakarta Sans', 'sans-serif']
+    },
+    fontSize: {
+      hS: '12px',
+      bL: '13px',
+      hM: '15px',
+      hL: '18px',
+      hXL: '24px',
+    },
+    lineHeight: {
+      S: '15px',
+      M: '19px',
+      L: '23px',
+      XL: '30px',
+    },
+    letterSpacing: {
+      S: '2.4px',
     },
   },
   plugins: [
