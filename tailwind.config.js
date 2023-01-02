e17652d401d5ed0cd758624314dd642f8c05da4b
Baradel-Clement <baradelclement@gmail.com>
2023-01-02 15:05:20 +0100

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./pages/*.{js,jsx,ts,tsx}", "./components/*.{ts,tsx}"],
+  content: ["./pages/*.{js,jsx,ts,tsx}", "./components/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
